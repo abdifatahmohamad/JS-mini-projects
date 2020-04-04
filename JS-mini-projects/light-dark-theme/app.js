d2f570ba32e5585cff72a423eb76ad5d0fa4c7af
Abdifatah Ali <abdifatahali@Abdifatahs-MacBook-Pro.local>
2020-04-04 01:46:52 -0500

    deleted JS-mini-projects folder

--- JS-mini-projects/light-dark-theme/app.js
+++
@@ -1,6 +0,0 @@
-const checkbox = document.getElementById('checkbox');
-
-checkbox.addEventListener('change', () => {
-	//Change the theme of the website
-	document.body.classList.toggle('dark');
-});
