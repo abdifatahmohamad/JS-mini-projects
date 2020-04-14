253f442404c889f1413003b0833ff37850c056b7
Abdifatah Ali <abdifatahali@Abdifatahs-MacBook-Pro.local>
2020-04-14 00:12:48 -0500

    added portfolio projects filter

---
+++ portfolio-projects-filter/app.js
@@ -0,0 +1,13 @@
+const btnContainer = document.querySelector('#filter-btn');
+
+// Get all buttons with class="btn" inside the container
+const btns = btnContainer.querySelectorAll('.btn');
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (let i = 0; i < btns.length; i++) {
+	btns[i].addEventListener('click', function () {
+		const current = document.getElementsByClassName('active');
+		current[0].className = current[0].className.replace(' active', '');
+		this.className += ' active';
+	});
+}
