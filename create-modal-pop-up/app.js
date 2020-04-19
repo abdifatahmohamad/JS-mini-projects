4e01eccff86813ebdb681155b1a4ce5fe0e3f6e7
Abdifatah Ali <48977326+abdifatahali@users.noreply.github.com>
2020-04-19 03:15:34 -0500

    Update app.js

--- create-modal-pop-up/app.js
+++ create-modal-pop-up/app.js
@@ -5,9 +5,11 @@ const openBtn = document.querySelector('.openBtn'),
 openBtn.addEventListener('click', () => {
 	modalContainer.classList.add('show');
 });
+
 closeIcon.addEventListener('click', () => {
 	modalContainer.classList.remove('show');
 });
+
 window.addEventListener('click', (e) => {
 	if (e.target === modalContainer) {
 		modalContainer.classList.remove('show');
