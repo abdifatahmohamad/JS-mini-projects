4452019029b8cf65860e9ccf441e57f486c67b5f
Abdifatah Mohamed <48977326+abdifatahdev@users.noreply.github.com>
2020-06-30 19:34:07 -0500

    Update app.js

--- portfolio-projects-filter/app.js
+++ portfolio-projects-filter/app.js
@@ -3,7 +3,7 @@ const btnContainer = document.querySelector('#filter-btn');
 // Get all buttons with class="btn" inside the container
 const btns = btnContainer.querySelectorAll('.btn');
 
-// Loop through the buttons and add the active class to the current/clicked button
+// Loop through the buttons and add the active class to the current/clicked button 
 for (let i = 0; i < btns.length; i++) {
 	btns[i].addEventListener('click', function () {
 		const current = document.getElementsByClassName('active');
